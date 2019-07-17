c3565ed16ad84dd160035a7d96f8a1ddb9c9cd27
sodegome <sodegome@espol.edu.ec>
2019-07-17 07:37:36 -0500

    Codigo QR

---
+++ app/src/main/java/com/example/guest_qr/GenerarQR.java
@@ -0,0 +1,132 @@
+package com.example.guest_qr;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.UUID;
+
+public class GenerarQR extends AppCompatActivity {
+
+    //private EditText eInput;
+    private Button btnCreateCode;
+    private ImageView imageView;
+    private Button btnShare;
+
+    private Bitmap bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generar_qr);
+
+        //eInput = findViewById(R.id.etInput);
+        btnCreateCode = findViewById(R.id.btnCreate);
+        imageView = findViewById(R.id.imageView);
+        btnShare = findViewById(R.id.btnShare);
+
+        String text = generateString();
+
+        if(text != null){
+            MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+            try{
+                BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE, 500,  500);
+                BarcodeEncoder barrBarcodeEncoder = new BarcodeEncoder();
+                bitmap = barrBarcodeEncoder.createBitmap(bitMatrix);
+                imageView.setImageBitmap(bitmap);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                /* Intent share = new Intent(Intent.ACTION_SEND);
+                share.setType("image/jpeg");
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG,100,byteArrayOutputStream);
+                File file = new File(Environment.getExternalStorageDirectory() + File.separator + "qr.jpg");
+                try{
+                    file.createNewFile();
+                    FileOutputStream fileOutputStream = new FileOutputStream(file);
+                    fileOutputStream.write(byteArrayOutputStream.toByteArray());
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+
+                share.putExtra(Intent.EXTRA_STREAM,Uri.parse("file:///sdcard/qr.jpg"));
+                startActivity(Intent.createChooser(share, "Share Image"));*/
+
+                try {
+
+                    File cachePath = new File(getApplicationContext().getCacheDir(), "images");
+                    cachePath.mkdirs(); // don't forget to make the directory
+                    FileOutputStream stream = new FileOutputStream(cachePath + "/image.png"); // overwrites this image every time
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                    stream.close();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                File imagePath = new File(getApplicationContext().getCacheDir(), "images");
+                File newFile = new File(imagePath, "image.png");
+                Uri contentUri = FileProvider.getUriForFile(getApplicationContext(), "com.example.guest_qr.fileprovider", newFile);
+
+                if (contentUri != null) {
+                    Intent shareIntent = new Intent();
+                    shareIntent.setAction(Intent.ACTION_SEND);
+                    shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); // temp permission for receiving app to read this file
+                    shareIntent.setDataAndType(contentUri, getContentResolver().getType(contentUri));
+                    shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
+                    startActivity(Intent.createChooser(shareIntent, "Choose an app"));
+                }
+            }
+        });
+
+    }
+
+    /*public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }*/
+
+    public String generateString() {
+        String uuid = UUID.randomUUID().toString();
+        uuid = uuid.substring(0,6);
+        return uuid.replace("-","");
+
+    }
+}
--- app/src/main/java/com/example/guest_qr/invitacionIndividual.java
+++ app/src/main/java/com/example/guest_qr/invitacionIndividual.java
@@ -39,15 +39,15 @@ public class invitacionIndividual extends AppCompatActivity {
         txtHoraHasta = (EditText) findViewById(R.id.txtHoraHasta);
         btnRegistro = (Button) findViewById(R.id.btnRegistro);
 
-        /*
-        //Llamada al menu
+
+        //Llamada a generar QR
         btnRegistro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (v.getContext(), listaInvitados.class);
+                Intent intent = new Intent (v.getContext(), GenerarQR.class);
                 startActivityForResult(intent, 0);
             }
-        });*/
+        });
 
         //Fecha Desde
         txtFechaDesde.setOnClickListener(new View.OnClickListener() {
