d957170328a414520a63f3fe61f65d2385be8058
sodegome <sodegome@espol.edu.ec>
2019-08-03 21:33:13 -0500

    Añadir splash screen

---
+++ app/src/main/java/com/example/guest_qr/Splash.java
@@ -0,0 +1,27 @@
+package com.example.guest_qr;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+    // Duración en milisegundos que se mostrará el splash
+    private final int DURACION_SPLASH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                // Cuando pasen los 3 segundos, pasamos a la actividad principal de la aplicación
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+
+    }
+}
